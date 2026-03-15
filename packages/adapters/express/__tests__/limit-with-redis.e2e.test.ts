66af696e9c5bf2bc26749b56d8a45e327bb62706
alphatrann <alphatran.forwork@gmail.com>
2026-03-15 16:09:04 +0700

    isolate redis db across tests

--- packages/adapters/express/__tests__/limit-with-redis.e2e.test.ts
+++ packages/adapters/express/__tests__/limit-with-redis.e2e.test.ts
@@ -13,7 +13,7 @@ describe("RedisStore", () => {
   async function createApp() {
     const app = express();
     redis = createClient({
-      url: "redis://localhost:6379/14",
+      url: "redis://localhost:6379/13",
     });
     await redis.connect();
     await redis.flushDb();
